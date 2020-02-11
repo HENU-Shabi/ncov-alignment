0b689b4b7604e62da874437cc74103452bf551f4
Chengeng_Lu <qq844303749@gmail.com>
2020-02-11 12:28:13 +0800

    bugfix

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-row no-gutters>
+        <v-col cols="12">
+          <v-app-bar
+                  app
+                  color="primary"
+                  dark
+                  dense
+          ></v-app-bar>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <Alignment/>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Alignment from "@/components/Alignment";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Alignment
+  },
+
+  data: () => ({
+    //
+  }),
+  mounted:function () {
+  }
+};
+</script>
